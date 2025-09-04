64bd42bb609691ed432588aca206a6b706b2b757
MannP05 <mannapatel06@gmail.com>
2025-09-04 16:41:34 -0400

    completed api requests and saves to data folder

--- dataCollection.py
+++ dataCollection.py
@@ -1,9 +1,12 @@
 import requests
 
 #sets api key from file 
-# API_KEY = open("API_KEY.txt").read().strip()
+API_KEY = open("API_KEY.txt").read().strip()
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
-response = requests.get("https://api.eoddata.com/Profile/List/NASDAQ?ApiKey=ziDMPXnCDE8brGlo61fG8nOn", headers=headers)
+response = requests.get(f"https://api.eoddata.com/Quote/List/NASDAQ?ApiKey={API_KEY}", headers=headers)
 response.raise_for_status()
-print(response.text)
+#print(response.text)
+
+with open("data/NASDAQ_list.txt", "w") as f: #write list of all nasdaq stocks to file
+    f.write(response.text)
