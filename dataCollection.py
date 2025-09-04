89bc0092810bef50eea9a581b5ea01e8fe435429
Kaven Muraleitharan <kavenmuraleitharan0726@gmail.com>
2025-09-04 14:02:39 -0400

    Added header with user agent to bypass the eoddata.com 403 forbidden access error

--- dataCollection.py
+++ dataCollection.py
@@ -1,17 +1,9 @@
 import requests
-import json
 
 #sets api key from file 
-API_KEY = open("API_KEY.txt").read().strip()
-
-data = requests.get(
-    "https://api.eoddata.com/Quote/List/NASDAQ",
-    params={
-      "ApiKey": API_KEY
-    }
-)
-
-
-with open("nasdaq_data.txt", "wb") as f: #save data as txt file 
-    f.write(data.content)
-print("Data saved to nasdaq_data.json") #print message 
+# API_KEY = open("API_KEY.txt").read().strip()
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+response = requests.get("https://api.eoddata.com/Profile/List/NASDAQ?ApiKey=ziDMPXnCDE8brGlo61fG8nOn", headers=headers)
+response.raise_for_status()
+print(response.text)
